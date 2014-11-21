59ca4fc074bf0bcf06954cc850231423a9746abb
Conor Hayes <ch1987@live.ie>
2014-11-21 18:20:02 +0000

    Added Main Class to run game

---
+++ src/strategy/ShootEmUpGame.java
@@ -0,0 +1,12 @@
+package strategy;
+
+public class ShootEmUpGame {
+    public static void main(String[] args) {
+        Character mainCharacter = new MainCharacter();
+        System.out.println("Main character is " + mainCharacter.walk());
+
+        Character enemy1 = new Enemy();
+        System.out.println("Enemy1 is " + enemy1.walk());
+
+    }
+}
