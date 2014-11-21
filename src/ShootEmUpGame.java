0f3e8ce6c920c71a1097cfc5cd7ea87e47546bba
Conor Hayes <ch1987@live.ie>
2014-11-21 22:50:20 +0000

    Tidy up Main

--- src/ShootEmUpGame.java
+++ src/ShootEmUpGame.java
@@ -1,3 +1,5 @@
+import factory.EnemyVehicleFactory;
+import factory.MainCharacterVehicleFactory;
 import factory.Vehicle;
 import factory.VehicleFactory;
 import strategy.*;
@@ -7,33 +9,43 @@ import javax.swing.*;
 
 public class ShootEmUpGame {
     public static void main(String[] args) {
-        strategy.Character mainCharacter = new MainCharacter();
-        System.out.println("Main character is " + mainCharacter.walk());
+        Character mainCharacter = new MainCharacter();
+        print("Main character is " + mainCharacter.walk());
 
         Character enemy1 = new Enemy();
-        System.out.println("Enemy1 is " + enemy1.walk());
+        print("Enemy1 is " + enemy1.walk());
 
-        System.out.println("Main character is " + mainCharacter.getShootBehaviour());
-        System.out.println("Enemy1 is " + enemy1.run());
+        print("Main character is " + mainCharacter.getShootBehaviour());
+        print("Enemy1 is " + enemy1.run());
 
-        System.out.println("Main character is " + mainCharacter.getGrenadeBehaviour());
+        print("Main character is " + mainCharacter.getGrenadeBehaviour());
 
         Character civilian1 = new Civilian();
-        System.out.println("Civilian1 is " + civilian1.run());
-        System.out.println("Civilian1 " + civilian1.getShootBehaviour());
+        print("Civilian1 is " + civilian1.run());
+        print("Civilian1 " + civilian1.getShootBehaviour());
 
         Character hostage1 = new Hostage();
-        System.out.println("Hostage1 is " + hostage1.walk());
-        System.out.println("Hostage1 " + hostage1.getGrenadeBehaviour());
+        print("Hostage1 is " + hostage1.walk());
+        print("Hostage1 " + hostage1.getGrenadeBehaviour());
 
-        System.out.println("Civilian 1 becomes Vigilante!");
+        print("Civilian 1 becomes Vigilante!");
         civilian1.setShootBehaviour(new ShootWithAK47());
-        System.out.println("Civilian1 " + civilian1.getShootBehaviour());
+        print("Civilian1 " + civilian1.getShootBehaviour());
 
-        VehicleFactory factory = new VehicleFactory();
-        String type = JOptionPane.showInputDialog("Enter Vehicle type (T for Tank or A for Armoured Car)");
-        Vehicle vehicle = factory.createVehicle(type);
-        System.out.println("Main character " + vehicle.drive());
-        System.out.println("Main character " + vehicle.shoot());
+        VehicleFactory enemyVehicleFactory = new EnemyVehicleFactory();
+        String type = JOptionPane.showInputDialog("Enter Enemy Vehicle type (T for Tank or A for Armoured Car)");
+        Vehicle vehicle = enemyVehicleFactory.createVehicle(type);
+        print("Enemy " + vehicle.drive());
+        print("Enemy " + vehicle.shoot());
+
+        VehicleFactory mainCharacterVehicleFactory = new MainCharacterVehicleFactory();
+        type = JOptionPane.showInputDialog("Enter Main Character Vehicle type (F for Flamethrower Tank or H for Helicopter)");
+        vehicle = mainCharacterVehicleFactory.createVehicle(type);
+        print("Main Character " + vehicle.drive());
+        print("Main Character " + vehicle.shoot());
+    }
+
+    public static void print(String output){
+        System.out.println(output);
     }
 }
