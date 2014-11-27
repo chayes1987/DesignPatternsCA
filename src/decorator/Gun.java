f46aa92f6559cd64687122df7df105b8deb28160
Conor Hayes <ch1987@live.ie>
2014-11-27 17:20:05 +0000

    'Added'

--- src/decorator/Gun.java
+++ src/decorator/Gun.java
@@ -1,7 +1,6 @@
 package decorator;
 
-public abstract class Gun {
-    public abstract void setDescription(String description);
-    public abstract String getDescription();
-    public abstract int getTotalAmmo();
+public interface Gun {
+
+    String getDescription();
 }
