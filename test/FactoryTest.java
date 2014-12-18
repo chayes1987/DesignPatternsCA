7407f7101be17709347dd547927f4ea1eb5a6889
Conor Hayes <ch1987@live.ie>
2014-12-18 00:18:33 +0000

    Added Test class for Factory

---
+++ test/FactoryTest.java
@@ -0,0 +1,26 @@
+import factory.IVehicle;
+import factory.VehicleFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.swing.*;
+
+/**
+ * Created by Conor on 18/12/2014.
+ */
+public class FactoryTest {
+    @Test
+    public void TestFactory(){
+        VehicleFactory enemyVehicleFactory = VehicleFactory.getVehicleFactory("Enemy");
+        IVehicle vehicle = enemyVehicleFactory.createVehicle("T");
+        Assert.assertNotEquals(null, vehicle);
+        vehicle = enemyVehicleFactory.createVehicle("X");
+        Assert.assertEquals(null, vehicle);
+
+        VehicleFactory mainCharacterVehicleFactory = VehicleFactory.getVehicleFactory("Main Character");
+        vehicle = mainCharacterVehicleFactory.createVehicle("F");
+        Assert.assertNotEquals(null, vehicle);
+        vehicle = enemyVehicleFactory.createVehicle("X");
+        Assert.assertEquals(null, vehicle);
+    }
+}
