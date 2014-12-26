f1a2a18ddbc919d60a0ba48f9fa67559b1786e35
Conor Hayes <ch1987@live.ie>
2014-12-26 01:07:43 +0000

    Edits Command Pattern tests to include Macro command and undo functionality

--- test/CommandTest.java
+++ test/CommandTest.java
@@ -1,6 +1,12 @@
 import command.*;
+import factory.ArmouredCar;
+import factory.Tank;
 import org.junit.Assert;
 import org.junit.Test;
+import strategy.Enemy;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandTest {
 
@@ -10,15 +16,28 @@ public class CommandTest {
         ICommand command = CheatAction.getAction("D", receiver);
         CheatCodeInvoker invoker = new CheatCodeInvoker(command);
         Assert.assertEquals("Enemy Tank has been destroyed", invoker.performCheat());
+        Assert.assertEquals("Tank has re-formed", invoker.undoCheat());
 
         receiver = CheatTarget.getTarget("E");
         command = CheatAction.getAction("C", receiver);
         invoker = new CheatCodeInvoker(command);
         Assert.assertEquals("Enemy has spontaneously combusted", invoker.performCheat());
+        Assert.assertEquals("Enemy has re-spawned!", invoker.undoCheat());
 
         receiver = CheatTarget.getTarget("A");
         command = CheatAction.getAction("W", receiver);
         invoker = new CheatCodeInvoker(command);
         Assert.assertEquals("Armoured car striped off Weaponry", invoker.performCheat());
+        Assert.assertEquals("Armoured car has re-added weaponry", invoker.undoCheat());
+
+        List<ICheatCodeReceiver> receiverList = new ArrayList<ICheatCodeReceiver>();
+        receiverList.add(new Enemy());
+        receiverList.add(new ArmouredCar());
+        receiverList.add(new Tank());
+
+        EnemyObliterationCheat cheat = new EnemyObliterationCheat(receiverList);
+        invoker = new CheatCodeInvoker(cheat);
+        Assert.assertEquals("Enemy has spontaneously combusted\nArmoured car spontaneously combusted\nEnemy Tank spontaneously combusted\n", invoker.performCheat());
+        Assert.assertEquals("Enemy has re-spawned!\nArmoured car has re-spawned!\nTank has re-spawned!\n", invoker.undoCheat());
     }
 }
